d817f2bac4b7ec89afaf90feb7c70a9ffedc38ae
Nicolas Rivas <nrivas2017@alu.uct.cl>
2018-10-25 13:22:41 -0300

    Actualizacion conexion DB
    script actualizado , incorporando schema

--- conexDB.js
+++ conexDB.js
@@ -1,8 +1,28 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
 
-var Gato = mongoose.model('Gato', { nombre: String });
+mongoose.connect('mongodb://localhost/test',function (err){ /* Conexion a la DB (test) */
+    if (err) throw err;
 
-var cuchito = new Gato({ name: 'cuchito' });
+    console.log("Conectado");
+});
 
-cuchito.save().then(() => console.log('miau'));
+var GatoSchema = mongoose.Schema({
+    Nombre: String,
+    Apellido: String,
+    Edad: Number
+});
+var Gato = mongoose.model('Gato', GatoSchema);
+
+var CuchoGato = new Gato({
+    Nombre: "Cucho",
+    Apellido: "Zapatilla",
+    Edad: 22
+});
+ 
+CuchoGato.save(function(err) {
+    if (err) throw err;
+     
+    console.log('Gato Guardado exitosamente');
+
+});
+
