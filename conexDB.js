1bc6ccfb283a6fc5531f4e31bdbe2295ef7860c3
mpedemonte <mpedemonte2017@alu.uct.cl>
2018-10-25 12:17:54 -0300

    Borrado para comenzar de nuevo

--- conexDB.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
-
-var Gato = mongoose.model('Gato', { nombre: String });
-
-var cuchito = new Gato({ name: 'cuchito' });
-
-cuchito.save().then(() => console.log('miau'));
