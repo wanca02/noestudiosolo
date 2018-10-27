2fbe0a4086828732cc3b2363ee2145cc4480c3b0
mpedemonte <mpedemonte2017@alu.uct.cl>
2018-10-27 18:06:23 -0300

    agregado tipos de find

--- mongoose_basics/index_find.js
+++ mongoose_basics/index_find.js
@@ -6,12 +6,32 @@ mongoose.connect('mongodb://localhost/noestudiosolo', function (err) {
    if (err) throw err;
  
    console.log('Conectado!');
+
+   
+   usuario_datos_cuenta.find({
+    correo: "mpedemonte2017@alu.uct.cl"
+    }).exec(function(err, datos) {
+    if (err) throw err;
  
-    usuario_datos_cuenta.find({
+    console.log(datos);
+
+    }); 
+    
+   usuario_datos_cuenta.findOne({
+     usuario: "maps",correo: "mpedemonte2017@alu.uct.cl"
+        }).exec(function(err, datos) {
+        if (err) throw err;
+     
+        console.log(datos);
+        console.log(datos.correo)    
+    }); 
+   
+   usuario_datos_cuenta.findOne({
         usuario: "mpedemonte"
-    }).exec(function(err, books) {
+    }).exec(function(err, datos) {
         if (err) throw err;
          
-        console.log(books);
+        console.log(datos.clave);
+    
     });
 });
