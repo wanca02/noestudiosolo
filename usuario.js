ff1077c872e76df92854c91ba3d8c08f0cb9196f
Nicolas Rivas <nrivas2017@alu.uct.cl>
2018-10-29 10:49:33 -0300

    act

--- usuario.js
+++ usuario.js
@@ -1,16 +1,60 @@
 var mongoose = require('mongoose');
+ 
 
-
-var UsuarioSchema = mongoose.Schema({
+var Datos_cuentaSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    puntos_actividad:{type: Number
-    },
-    valoracion_plataforma:{type: Number        
+    Datos_cuenta:{usuario:{type: String,
+                            required: true    
+                            },
+                  clave:{type: String,
+                  required: true        
+                            },
+                  nombre:{type: String
+                            },
+                  correo: {type: String
+                         },
+                fecha_registro: {type: Date,
+                                default: Date.now
+                        },
+                nivel_permisos:{type: String
+                        },
+                ultimo_ingreso:{type: Date,
+                        default: Date.now
+                    }
+                },
+    Datos_personales:{nombre:{type: String,
+                      required: true    
+                    },
+                      fecha_nacimiento:{type: Date        
+                    },
+                     pais:{type: String
+                    },
+                    residencia: {type: String
+                    },
+                    genero: {type: String
+                    },
+                    nivel_educativo: {type: String
+                    },
+                    carrera: {type: String
+                    },
+                    rol: {type: String
+                    },
+                    disciplina: {type: String
+                    }
+                },
+        Computo:{nsesiones_terminadas:{ type: Number    
+                    },
+                    nactividades_terminadas:{ type: Number
+                    },
+                    nsesiones_no_terminadas:{ type: Number
+                    },
+                    nactividades_no_terminadas:{ type: Number
+        }
     }
-    
 });
 
 
-var Usuario = mongoose.model('Usuario',UsuarioSchema);
+ 
+var datos_cuenta = mongoose.model('Datos_cuenta', Datos_cuentaSchema);
 
-module.exports = Usuario;
+module.exports = datos_cuenta;
